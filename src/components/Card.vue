2817bfb7f4d6aa930d0bb8832dc3c6c326599ea5
songji1165 <songji1165@gmail.com>
2019-04-27 23:38:32 +0900

    card 중첩라우터 추가

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Card 
+        <div>
+            {{cid}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            cid : 0
+        }
+    },
+    watch : {
+        //라우트 감시하고 있다가 cid 변경시 작동
+        '$route' () {
+            this.cid = this.$route.params.cid
+        }
+    },
+    created () { // 처음 한 번 갱신되고나서 변하지 않는다.
+        this.cid = this.$route.params.cid
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,39 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
-import NotFound from '../components/NotFound.vue'
 import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
+import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login},
-  { path: '*', component: NotFound },
-  { path: '/b/:bid', component: Board }
-  // :bid 라는 변수를 받을 수 있다
+const routes = [{
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/b/:bid',
+    component: Board,
+    children: [ //하위경로 지정 http://localhost:8080/b/1/c/1 중첩라우터
+      {
+        path: 'c/:cid',
+        component: Card
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: NotFound
+  },
 ]
 
 
 const router = new VueRouter({
-  mode:'history',
+  mode: 'history',
   routes: routes
 })
 
